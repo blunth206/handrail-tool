d8bf2277a6f19236e425785275ba68206f73ca7a
blunth206 <303979338@qq.com>
2026-07-25 20:30:28 +0800

    feat: 扶手长度重组工具 - 最优组合方案计算器

---
+++ server.js
@@ -0,0 +1,23 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+const os = require('os');
+
+const PORT = 2026;
+const mime = { 'html': 'text/html', 'js': 'text/javascript', 'css': 'text/css' };
+
+http.createServer((req, res) => {
+  const f = path.join(__dirname, req.url === '/' ? 'index.html' : req.url);
+  const ext = path.extname(f).slice(1);
+  fs.readFile(f, (err, data) => {
+    if (err) { res.writeHead(404); res.end('Not found'); return; }
+    res.writeHead(200, { 'Content-Type': mime[ext] || 'text/plain', 'Cache-Control': 'no-cache' });
+    res.end(data);
+  });
+}).listen(PORT, () => {
+  console.log('Local: http://localhost:' + PORT);
+  const ifaces = os.networkInterfaces();
+  Object.values(ifaces).forEach(info => {
+    info.forEach(d => { if (d.family === 'IPv4' && !d.internal) console.log('Mobile: http://' + d.address + ':' + PORT); });
+  });
+});
